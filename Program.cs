5d08e2573f2c15ec76528956a1dd53418f24ec4e
nirzaf <mfmfazrin1986@gmail.com>
2022-02-16 08:58:06 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class MathProblem{
+
+//find all numbers and add into array from a string input
+public static int[] FindNumbers(string input){
+  int[] numbers = new int[input.Length];
+  for(int i = 0; i < input.Length; i++){
+    numbers[i] = int.Parse(input[i].ToString());
+  }
+  return numbers;
+}
+
+}
